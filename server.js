7854da72ccaaeb37e229b1a19b8525b8493dd9b0
joantolos <joan.tolos@gmail.com>
2021-01-04 15:09:31 +0100

    Package json configuration and express server to deploy on Heroku

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-'use strict';
-/*jshint esversion: 6 */
-
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080, function(){
-  console.log('Server started at http://localhost:8080/');
+// Serve static files....
+app.use(express.static(__dirname + '/dist/kata-lazy-oven'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/kata-lazy-oven/index.html'));
 });
 
+// default Heroku PORT
+app.listen(process.env.PORT || 8080);
