b82465282a949a90538845e2caf30bfba85c5bc1
joantolos <joan.tolos@gmail.com>
2021-01-07 11:02:46 +0100

    Using combos from angular material to select meal time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SettingsComponent } from './components/settings/settings.component';
-import { RecipeComponent } from './components/steps/recipe.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { RecipeComponent } from './components/recipe/recipe.component';
 import { LazyOvenService } from './services/lazy-oven.service';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,14 +19,16 @@ import { LazyOvenService } from './services/lazy-oven.service';
     RecipeComponent
   ],
   imports: [
-    ReactiveFormsModule,
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       {path: 'settings', component: SettingsComponent},
-      {path: 'steps', component: RecipeComponent},
+      {path: 'recipe', component: RecipeComponent},
       {path: '', redirectTo: '/settings', pathMatch: 'full'},
       {path: '**', component: PageNotFoundComponent}
     ]),
+    MatSelectModule,
+    FormsModule,
   ],
   providers: [LazyOvenService],
   bootstrap: [AppComponent]
--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Settings } from '../../entities/settings';
 import { LazyOvenService } from '../../services/lazy-oven.service';
 import { Time } from "../../entities/time";
+import { defaultSettings } from '../../config/config';
+import { hours } from '../../config/config';
+import { minutes } from '../../config/config';
 
 @Component({
   selector: 'app-settings',
@@ -10,17 +13,22 @@ import { Time } from "../../entities/time";
 })
 export class SettingsComponent implements OnInit {
 
+  selectedEatHours: number = defaultSettings.eat.hours;
+  selectedEatMinutes: number = defaultSettings.eat.minutes;
+  hours = hours;
+  minutes = minutes;
+
   constructor(private lazyOvenService: LazyOvenService) { }
 
   ngOnInit(): void {
   }
 
-  add(eatHours: number, eatMinutes: number, cookHours: number, cookMinutes: number, heat: number, cool: number): void {
+  add(): void {
     const settings = new Settings(
-      new Time(eatHours, eatMinutes),
-      new Time(cookHours, cookMinutes),
-      heat,
-      cool);
+      new Time(this.selectedEatHours, this.selectedEatMinutes),
+      new Time(this.selectedEatHours, this.selectedEatMinutes),
+      defaultSettings.heat,
+      defaultSettings.cool);
 
     this.lazyOvenService.addSettings(settings);
   }
